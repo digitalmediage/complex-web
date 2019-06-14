af8ff32718d18754c0d3aaf932af301ca37385d0
Shima Ghasemi <shimaghasemi36@gmail.com>
2019-06-14 12:04:43 +0430

    add sign in page

---
+++ app/containers/Layout/Footer/index.js
@@ -0,0 +1,152 @@
+/* eslint-disable react/jsx-no-duplicate-props */
+/* eslint-disable react/prop-types */
+/* eslint-disable import/named */
+/*
+ * ExecFileOptionsWithBufferEncoding
+ *
+ */
+
+import React from 'react';
+
+class ExecFileOptionsWithBufferEncoding extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <footer className="footer-box">
+        <div className="footer">
+          <ul>
+            <li className="footer-social">
+              <i className="fas fa-map-marker-alt footer-pin" /> No. 13, Geo
+              Blv. , Tiblisi , Georgia
+            </li>
+            <li className="footer-social">
+              <i className="fas fa-phone-volume footer-phone" /> +995 2233445566
+            </li>
+            <li className="footer-social">
+              <i className="fas fa-envelope footer-mail" />
+              info@gerproperties.ge
+            </li>
+            <li className="social-box">
+              <i className="fab fa-facebook-f social-icon facebook" />
+              <i className="fab fa-instagram social-icon" />
+              <i className="fab fa-linkedin social-icon" />
+              <p>(C) Gqo properties</p>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-text-box">
+              <a href="/" className="footer-title" href="">
+                About Us
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Mission
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Partners
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Team
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Certificates
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Facts & Figures
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-text-box">
+              <a href="/" className="footer-title" href="">
+                Services
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Consulting
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Valuation
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Advisory
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Market Analysis
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Business Planning
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Legal Services
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li className="footer-text-box">
+              <a href="/" className="footer-title" href="">
+                Projects
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Residental
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Commercial
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Industrial
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Complexes
+              </a>
+            </li>
+            <li className="footer-text-box">
+              <a href="/" className="footer-text" href="">
+                Urban Deelopment
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default ExecFileOptionsWithBufferEncoding;
