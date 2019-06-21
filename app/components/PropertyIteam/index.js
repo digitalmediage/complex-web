dc98f2927cfd6cfc488e5337fb537ed837f8f420
hamidReazeNikoonia <hamidrezanik00nia@gmail.com>
2019-06-22 04:24:44 +0430

    PropertyIteam updated

--- app/components/PropertyIteam/index.js
+++ app/components/PropertyIteam/index.js
@@ -1,54 +1,64 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/prop-types */
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useState} from 'react';
 import cx from 'classnames';
 import styles from '../../containers/Home/styles.css';
 
 import likeBlack from '../../images/like-black.png';
-// import like from '../../images/like.png';
+import like from '../../images/like.png';
 import bed from '../../images/bed.png';
 import bathtub from '../../images/bathtub.png';
 
-const PropertyIteam = () => (
-  <React.Fragment>
-    <div className={cx(styles.property, 'mr-3 mt-5')}>
-      <div className={styles.propertiesImg}>
-        <img className={styles.heartImg} alt="img" src={likeBlack} />
-        <img
-          className={cx(styles.activeHeart, 'img-fluid')}
-          alt="img"
-          src={likeBlack}
-        />
-      </div>
-      <div className={styles.propertiesDetail}>
-        <p className={styles.propertiesSecondTitle}>
-          Apartment
-          <span className={styles.propertiesText}>
-            280 m<sup>2</sup>
-          </span>
-        </p>
-        <h5>$ 68,000</h5>
-        <p className={styles.propertiesLocation}>Saburtalo , Tbilisi</p>
-      </div>
-      <div className={styles.propertiesServices}>
-        <div className={styles.serviceBox}>
+const PropertyIteam = (props) => {
+  
+  const [liked, toggleLike] = useState('true');
+
+  return (
+
+  
+    <React.Fragment>
+      <div className={cx(styles.property, 'mr-3 mt-5')}>
+        <div className={styles.propertiesImg}>
           <img
-            className={cx(styles.serviceImg, 'img-fluid')}
+            onKeyPress={() => toggleLike(!liked)}
+            className={cx(styles.activeHeart, 'img-fluid')}
             alt="img"
-            src={bed}
+            onClick={() => toggleLike(!liked)}
+            src={liked ? like : likeBlack}
           />
-          <p className={styles.serviceText}>3 Bedrooms</p>
         </div>
-        <div className={styles.serviceBox}>
-          <img
-            className={cx(styles.serviceImg, 'img-fluid')}
-            alt="img"
-            src={bathtub}
-          />
-          <p className={styles.serviceText}>3 Bathrooms</p>
+        <div className={styles.propertiesDetail}>
+          <p className={styles.propertiesSecondTitle}>
+            { props.type }
+            <span className={styles.propertiesText}>
+              {props.size} m<sup>2</sup>
+            </span>
+          </p>
+          <h5>{ props.data.price ? props.data.price.$numberDecimal : null   }</h5>
+          <p className={styles.propertiesLocation}>{props.data.complex.country} , {props.data.complex.city}</p>
+        </div>
+        <div className={styles.propertiesServices}>
+          <div className={styles.serviceBox}>
+            <img
+              className={cx(styles.serviceImg, 'img-fluid')}
+              alt="img"
+              src={bed}
+            />
+            <p className={styles.serviceText}>3 Bedrooms</p>
+          </div>
+          <div className={styles.serviceBox}>
+            <img
+              className={cx(styles.serviceImg, 'img-fluid')}
+              alt="img"
+              src={bathtub}
+            />
+            <p className={styles.serviceText}>3 Bathrooms</p>
+          </div>
         </div>
       </div>
-    </div>
-  </React.Fragment>
-);
+    </React.Fragment>
+  )};
+
 
 export default PropertyIteam;
